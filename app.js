8a76a829e8c33be8627e3ff615c20d28d69e3471
Asa Smith <smith.asa.la@gmail.com>
2017-04-30 16:50:54 -0400

    :rocket:

---
+++ app.js
@@ -0,0 +1,18 @@
+const htmlStandards = require('reshape-standard')
+const cssStandards = require('spike-css-standards')
+const jsStandards = require('spike-js-standards')
+const pageId = require('spike-page-id')
+
+module.exports = {
+  devtool: 'source-map',
+  matchers: {
+    html: '*(**/)*.sgr',
+    css: '*(**/)*.sss'
+  },
+  ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
+  reshape: htmlStandards({
+    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } }
+  }),
+  postcss: cssStandards(),
+  babel: jsStandards()
+}
