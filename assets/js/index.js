2263829bc7a1ce0be5f128058d0184af0d64ec13
Asa Smith <smith.asa.la@gmail.com>
2017-05-06 18:08:28 -0400

    add hero statement

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,26 @@
 console.log('you can use ES6 here : )')
+
+const app = {
+  text : "<h1>hello, world</h1>",
+  index : 0,
+  chars : 0,
+  speed: 200,
+  container : '#hero',
+  init : function() {
+      this.chars = this.text.length;
+      return this.write();
+    },
+  write : function() {
+    let container = document.querySelector(this.container);
+    container.append(this.text[this.index]);
+      console.log('current character :', this.text[this.index]);
+      if (this.index < this.chars-1) {
+            this.index++;
+            setTimeout(function() {
+	            app.write();
+	          }, this.speed);
+          }
+    }
+};
+
+app.init();
