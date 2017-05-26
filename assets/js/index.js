620bfd4f90b642a1d69af13648ef5b1ac87f6cdb
Asa Smith <smith.asa.la@gmail.com>
2017-05-26 07:23:58 -0400

    lint js

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,29 +9,25 @@ var log = {
 log.console()
 
 const app = {
-  text : "hello world",
-  index : 0,
-  chars : 0,
+  text: 'hello world',
+  index: 0,
+  chars: 0,
   speed: 300,
-  container : '#hero',
-  init : function() {
-      this.chars = this.text.length;
-      return this.write();
-    },
-  write : function() {
-    let container = document.querySelector(this.container);
-    
-    container.textContent += this.text[this.index];
-      console.log('current character :', this.text[this.index]);
-      if (this.index < this.chars-1) {
-	    this.index++;
-	    setTimeout(function() {
-		    app.write();
-		  }, this.speed);
-	  }
-    }
-};
+  container: '#hero',
+  init: function () {
+    this.chars = this.text.length
+    return this.write()
+  },
+  write: function () {
+    let container = document.querySelector(this.container)
 
-window.onload = function () {
-  app.init();
+    container.textContent += this.text[this.index]
+    console.log('current character :', this.text[this.index])
+    if (this.index < this.chars - 1) {
+      this.index++
+      setTimeout(function () {
+        app.write()
+      }, this.speed)
+    }
+  }
 }
