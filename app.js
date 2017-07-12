3eac733b662a4940d23b4f0657ac6886606d8813
Asa Smith <smith.asa.la@gmail.com>
2017-07-12 11:21:23 -0400

    add dato cms

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
-const jsStandards = require('spike-js-standards')
+const jsStandards = require('babel-preset-env')
+const dynamicImport = require('babel-plugin-syntax-dynamic-import')
 const pageId = require('spike-page-id')
+const SpikeDatoCMS = require('spike-datocms')
+const locals = {}
 
 module.exports = {
   devtool: 'source-map',
@@ -11,9 +14,21 @@ module.exports = {
   },
   ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock'],
   reshape: htmlStandards({
-    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } }
+    locals: Object.assign(
+      locals
+    )
   }),
+  plugins: [
+    new SpikeDatoCMS({
+      addDataTo: locals,
+      token: 'd552423567ed1794c6a1',
+      models: [{
+        name: 'about',
+        json: 'about.json'
+      }],
+    })
+  ],
   postcss: cssStandards(),
-  babel: jsStandards(),
+  babel: { presets: [[jsStandards, { modules: false }]], plugins: [dynamicImport] },
   server: {open: false}
 }
