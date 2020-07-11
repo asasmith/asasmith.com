6ad6c73b3528bf37b15780a909b6ae5f519bce62
asasmith <smith.asa.la@gmail.com>
2020-07-11 18:26:57 -0400

    setup testing, basic test

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,24 @@
-import { Link } from 'gatsby'
-import React from 'react'
+import { Link } from 'gatsby';
+import React from 'react';
 
-export default function Header ({ siteTitle }) {
-    return (
-        <header 
-            style={{
-                margin: `2rem auto`,
-            }}>
-            <h1>
-                <Link 
-                    to='/'
-                    style={{
-                        color: `#0c0c0c`,
-                        textDecoration: `none`,
-                    }}
-                 >
-                    {siteTitle}
-                </Link>
-            </h1>
-        </header>
-    )
-} 
+export default function Header({ siteTitle }) {
+	return (
+		<header
+			style={{
+				margin: `2rem auto`,
+			}}
+		>
+			<h1>
+				<Link
+					to="/"
+					style={{
+						color: `#0c0c0c`,
+						textDecoration: `none`,
+					}}
+				>
+					{siteTitle}
+				</Link>
+			</h1>
+		</header>
+	);
+}
